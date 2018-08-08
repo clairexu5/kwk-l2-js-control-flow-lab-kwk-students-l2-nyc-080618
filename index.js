33b898b2954c5d65ac540bd807b52789d1fea8e7
Claire Xu <claire02.xu@gmail.com>
2018-08-08 20:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function scuberGreetingForFeet(ride) {
 }
 
 function ternaryCheckCity(){
-  var city = (city == "nyc") ? "Ok, sounds good.": "No, go.";
+  var city = (city == "NYC") ? "Ok, sounds good.": "No, go.";
   
   
 }
