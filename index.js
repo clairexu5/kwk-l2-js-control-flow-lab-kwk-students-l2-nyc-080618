071a678e7c3ea513d036f19d73293ddbeb04fb0e
Claire Xu <claire02.xu@gmail.com>
2018-08-08 20:15:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride > 2500){
+    return "No can do."
+  }
+  if (ride <= 400){
+    return "This one is on me!";
+  }if(ride > 2000 ){
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity(){
+  
+}
